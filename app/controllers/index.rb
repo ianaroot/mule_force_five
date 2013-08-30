9c5a293e5bdef969b8ab44319bc0d76959e3f7ae
Thomas Landon <thomasrlandon@gmail.com>
2013-08-30 15:53:40 -0700

    adding route skeletons

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,37 @@
+# home page
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+# create new user
+get '/new' do
+
+  erb :create_new_user
+end
+
+get '/:user_id' do
+
+  erb :user_home_page
+end
+
+post '/' do
+
+  redirect '/:user_id'
+end
+
+
+post '/new' do
+  erb :user_home_page
+end
+
+
+
+
+
+
+
+
+
+
+
